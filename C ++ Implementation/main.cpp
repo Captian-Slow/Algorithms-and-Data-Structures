bb17a29fe14fa08dc523de609f801fed8ddc10fd
thebournegenius <theamit97@gmail.com>
2017-02-12 16:47:44 +0530

    Add files via upload

---
+++ C ++ Implementation/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include"linkedList.h"
+
+int main()
+{
+    linkedList list;
+    list.insertLink(1);
+    list.insertLink(2);
+    list.insertLink(3);
+    list.insertLink(4);
+    list.insertLink(5);
+    list.removeLinkAt(3);
+
+    list.displayList();
+    return 0;
+}
