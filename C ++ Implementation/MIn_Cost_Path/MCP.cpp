aeb069445c417eb7cddc22fc2051efdcb15d9b5a
thebournegenius <theamit97@gmail.com>
2017-04-18 11:06:06 +0530

    Add files via upload

--- C ++ Implementation/MIn_Cost_Path/MCP.cpp
+++ C ++ Implementation/MIn_Cost_Path/MCP.cpp
@@ -2,49 +2,69 @@
 
 using namespace::std;
 
-int min_num(int a, int b, int c)
+int min_num(int x, int y, int z)
 {
 
-    int mini = a;
-
-    if(b < a)
-    {
-        mini = b;
-        if(c < b)
-            mini = c;
-    }
-
-    if(c < a)
-    {
-        mini = c;
-        if(b < c)
-            mini = b;
-    }
-
-    return mini;
+   if (x < y)
+      return (x < z)? x : z;
+   else
+      return (y < z)? y : z;
 
 }
 
-int minCost(int **a, int m, int n, int i, int j)
+int minCostExpo(int a[3][3], int i, int j)   //Exponential runtime.
 {
 
-    if(i < m && j < n)
-        return min_num(minCost(a, m, n, i + 1, j), minCost(a, m, n, i, j + 1), minCost(a, m, n, i + 1, j + 1));
+    //cout<<"\n"<<a[i][j];
+    if(i == 2 && j == 2)
+        return a[i][j];
+
+    if(i < 3 && j < 3)
+        return a[i][j] + min_num(minCostExpo(a, i + 1, j), minCostExpo(a,i, j + 1), minCostExpo(a, i + 1, j + 1));
+}
+
+int minCost(int a[3][3],int m,int n)
+{
+
+    int b[3][3];
+
+    b[0][0] = a[0][0];
+
+    for(int i = 1; i < 3; i++)
+    {
+        b[i][0] = b[i - 1][0] + a[i][0];
+    }
+
+    for(int i = 1; i < 3; i++)
+    {
+        b[0][i] = b[0][i - 1] + a[0][i];
+    }
+
+    for(int i = 1; i <= m; i++)
+    {
+
+        for(int j = 1; j <= n; j++)
+        {
+
+            b[i][j] = min_num(b[i - 1][j - 1], b[i - 1][j], b[i][j - 1]) + a[i][j];
+        }
+    }
+
+
+    return b[2][2];
 }
 
 int main()
 {
 
-    int a[50][50], i, j, m, n;
-
-    cout<<"Enter the dimensions: "<<m<<n;
+    int a[3][3], i, j;
 
     cout<<"Enter the array: \n";
 
-    for(i = 0; i < m; i++)
+    for(i = 0; i < 3; i++)
     {
 
-        for(j = 0; j < n; j++)
+        for(j = 0; j < 3; j++)
         {
 
             cin>>a[i][j];
@@ -53,7 +73,8 @@ int main()
         cout<<"\n";
     }
 
-    cout<<minCost(&&a, m, n, 0, 0);
+    cout<<"In exponential runtime: "<<minCostExpo(a, 0, 0);
+    cout<<"\nIn linear runtime: "<<minCost(a, 2, 2);
 }
 
 
